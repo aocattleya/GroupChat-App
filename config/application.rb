c40a231020d60ccbbb0433a5d6e37d32833fde17
aocattleya <aocattleya@yahoo.co.jp>
2019-11-04 10:47:58 +0900

    initial commit

---
+++ config/application.rb
@@ -0,0 +1,18 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module ChatSpace
+  class Application < Rails::Application
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
+  end
+end
