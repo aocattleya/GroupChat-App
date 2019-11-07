756d31befacd106fb217ae63a417d3045c18b118
aocattleya <aocattleya@yahoo.co.jp>
2019-11-07 16:23:29 +0900

    add: ユーザー情報編集機能を実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index"
+  resources :users, only: [:edit, :update]
 end
