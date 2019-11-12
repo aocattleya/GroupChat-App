d8095b76ae13f4d947a18d4e376e81596d929fe0
aocattleya <aocattleya@yahoo.co.jp>
2019-11-12 17:03:27 +0900

    add: 投稿機能のテストを追加

---
+++ spec/controllers/messages_controller_spec.rb
@@ -0,0 +1,92 @@
+require 'rails_helper'
+
+describe MessagesController do
+  let(:group) { create(:group) }
+  let(:user) { create(:user) }
+
+  describe '#index' do
+
+    context 'log in' do
+      before do
+        login user
+        get :index, params: { group_id: group.id }
+      end
+
+      it 'assigns @message' do
+        expect(assigns(:message)).to be_a_new(Message)
+      end
+
+      it 'assigns @group' do
+        expect(assigns(:group)).to eq group
+      end
+
+      it 'redners index' do
+        expect(response).to render_template :index
+      end
+    end
+
+    context 'not log in' do
+      before do
+        get :index, params: { group_id: group.id }
+      end
+
+      it 'redirects to new_user_session_path' do
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
+
+  describe '#create' do
+    let(:params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message) } }
+
+    context 'log in' do
+      before do
+        login user
+      end
+
+      context 'can save' do
+        subject {
+          post :create,
+          params: params
+        }
+
+        it 'count up message' do
+          expect{ subject }.to change(Message, :count).by(1)
+        end
+
+        it 'redirects to group_messages_path' do
+          subject
+          expect(response).to redirect_to(group_messages_path(group))
+        end
+      end
+
+      context 'can not save' do
+        let(:invalid_params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message, content: nil, image: nil) } }
+
+        subject {
+          post :create,
+          params: invalid_params
+        }
+
+        it 'does not count up' do
+          expect{ subject }.not_to change(Message, :count)
+        end
+
+        it 'renders index' do
+          subject
+          expect(response).to render_template :index
+        end
+      end
+    end
+
+    context 'not log in' do
+
+      it 'redirects to new_user_session_path' do
+        post :create, params: params
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+end
+
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,6 +33,9 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include ControllerMacros, type: :controller
   config.include FactoryBot::Syntax::Methods
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
