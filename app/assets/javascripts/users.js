0cc9b798bb255a3cd1dc8f84f16d0a7d0c2c9f06
aocattleya <aocattleya@yahoo.co.jp>
2019-11-23 02:17:21 +0900

    clean: users.jsのフォーマット

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,22 +1,23 @@
 $(function() {
   // HTML
   const addUser = (user) => {
-    let html = `<div class="chat-group-user clearfix">
-      <p class="chat-group-user__name">${user.name}</p>
-      <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
-    </div>
-    `;
+    let html =
+      `<div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">${user.name}</p>
+        <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+      </div>`;
     $("#user-search-result").append(html);
   };
   const addNoUser = () => {
-    let html = `<div class="chat-group-user clearfix">
-      <p class="chat-group-user__name">ユーザーが見つかりません</p>
-    </div>`;
+    let html =
+      `<div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">ユーザーが見つかりません</p>
+      </div>`;
     $("#user-search-result").append(html);
   };
   const addDeleteUser = (name, id) => {
-    let html = `
-    <div class="chat-group-user clearfix" id="${id}">
+    let html =
+    `<div class="chat-group-user clearfix" id="${id}">
       <p class="chat-group-user__name">${name}</p>
       <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</div>
     </div>`;
@@ -26,7 +27,6 @@ $(function() {
     let html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
     $(`#${userId}`).append(html);
   };
-
   // event
   $("#user-search-field").on("keyup", () => {
     let input = $("#user-search-field").val();
