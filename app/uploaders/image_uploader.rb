9c3732dcb48dc9123fb56f2f82c714583d79fee0
aocattleya <aocattleya@yahoo.co.jp>
2019-11-22 17:27:59 +0900

    add: fog-awsを導入

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,8 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   process resize_to_fit: [800, 800]
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
